0a883f8b09b008337cc056b64c26851129588b17
Ayenko4all <kenny4all40@gmail.com>
2020-06-23 18:07:33 -0700

    'update'

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,30 @@
   </div>
   <div class="card card-default">
     <div class="card-header">Posts</div>
-    <div class="card-body"></div>
+    <div class="card-body">
+      <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($posts as $post)
+        <tr>
+          <td><img src="storage/{{ $post->image }}" alt="" width="80px" height="60px"></td>
+          <td>{{ $post->title}}</td>
+          <td>{{ $post->description}}</td>
+          <td>
+            <a href="" class="btn btn-info btn-sm">Edit</a>
+            <a href="" class="btn btn-danger btn-sm">Trash</a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+      </table>
+    </div>
   </div>
 @endsection
